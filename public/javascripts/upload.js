9b4c066c30e8a661f177a256b1fc72c2e83e3a44
Upekka <Upekka>
2017-02-17 08:26:34 +0530

    modified info.json and upload files

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -1,11 +1,11 @@
-//Upload 
+//Upload image, 3D model and info.json to the server
 $('.upload-btn').on('click', function (){
     $('#progress-bar').text('0%');
     $('#progress-bar').width('0%');
 
   //var o = {"items":[]}; // create an object with key items to hold array
       var obj = {
-        distance :  parseFloat($('#distance').val()), // place the url in a new object
+        Distance :  parseFloat($('#distance').val()), // place the url in a new object
         x : parseFloat($('#x').val()), // place the name in a new object
         y : parseFloat($('#y').val()), 
         z : parseFloat($('#z').val()), 
@@ -16,7 +16,7 @@ $('.upload-btn').on('click', function (){
       
     // strigify data to JSON format
     var infoJsonString = JSON.stringify(obj, undefined, '\t');
-    console.log(infoJsonString);
+    //console.log(infoJsonString);
 
   var files_image = $('#upload-input-image').get(0).files;
   var files_model = $('#upload-input-model').get(0).files;
@@ -54,33 +54,7 @@ $('.upload-btn').on('click', function (){
       success: function(data){
           console.log('upload image successful!\n' + data);
       },
-      /*xhr: function() {
-        // create an XMLHttpRequest
-        var xhr = new XMLHttpRequest();
-
-        // listen to the 'progress' event
-        xhr.upload.addEventListener('progress', function(evt) {
-
-          if (evt.lengthComputable) {
-            // calculate the percentage of upload completed
-            var percentComplete = evt.loaded / evt.total;
-            percentComplete = parseInt(percentComplete * 100);
-
-            // update the Bootstrap progress bar with the new percentage
-            $('#progress-bar').text(percentComplete + '%');
-            $('#progress-bar').width(percentComplete + '%');
-
-            // once the upload reaches 100%, set the progress bar text to done
-            if (percentComplete === 100) {
-              $('#progress-bar').html('Done');
-            }
-
-          }
-
-        }, false);
-
-        return xhr;
-      }*/
+      
     });
 
     $.ajax({
@@ -120,14 +94,6 @@ $('.upload-btn').on('click', function (){
         return xhr;
       }
     });
-
-    /*$.ajax({
-      url: '/upload_info',
-      type: 'POST',
-      data: infoJsonString,
-      dataType: "json"
-  
-    });*/
 
   }
 });
